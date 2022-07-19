0ac08b916817c9f7bfc7ed98a23c5b3da3bba285
Melissa Modesto <mbm.modesto@gmail.com>
2022-07-19 15:15:14 -0300

    Initial Screen

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import InitialScreen from './InitialScreen';
+
+export default function App () {
+    return (
+        <div>
+            <InitialScreen />
+        </div>
+    )
+}
+
---
+++ src/components/InitialScreen.js
@@ -0,0 +1,9 @@
+export default function InitialScreen() {
+    return (
+        <div class="initial-screen">
+            <center><img src="./image/logo.png" alt="logo" /></center>
+            <h3>ZapRecall</h3>
+            <div class="button">Iniciar Recall!</div>
+        </div>
+    )
+}
